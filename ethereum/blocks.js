30c0715e44d4126ce4f6ca0158d6ff1cdbb1ea4e
Divyasshree <124589840+divyasshree-BQ@users.noreply.github.com>
2026-01-02 12:51:10 +0530

    Create blocks.js

---
+++ ethereum/blocks.js
@@ -0,0 +1,38 @@
+ // S3 url: https://bitquery-blockchain-dataset.s3.us-east-1.amazonaws.com/ethereum/blocks/24053500_24053549.parquet
+    //     ├── 24053500_24053549.parquet
+    // │   ├── 24053550_24053599.parquet
+    // │   ├── 24053600_24053649.parquet
+    // │   ├── 24053650_24053699.parquet
+    // │   ├── 24053700_24053749.parquet
+    // │   ├── 24053750_24053799.parquet
+    // │   ├── 24053800_24053849.parquet
+    // │   ├── 24053850_24053899.parquet
+    // │   ├── 24053900_24053949.parquet
+    // │   └── 24053950_24053999.parquet
+
+[
+  {
+    "Block_BaseFee":"0.000000000024022089",
+    "Block_BaseFeeInUSD":0.0000000716,
+    "Block_Bloom":"0xf6775b6f6e7ddfbdf5ff5fb3dfc1dfd74fff8f517df6f279d3a5b117dc7edfdeff0fafbfedfe8e9ed7fffb7777ff8d7bfbffe6ffdebd7494afd3f7fdcfedfdfeffd6dd7ffef6dc6f79bde3eb7edf6bb5b307fcf7dfff6e3a13bfff76f7b9bc663d7f7fbeff7fbffffd3af565d9b27fff5eee5f72ddee9f9e3dbf5b7feeedfbf59f2d6f6bf8f62f7d29f9ff6fdf7fabffbdf36bf57f4fbeae3fef7dfbf7fe3a7fdbdf2f6fb5fcb12efde7fbfabd74dc51bcffc9c5e7f17e2785f9f3247af5bbfbbb235faf75ef6b7d370f6ff9e5fbfcaecc7ffeff8fff5cf4ed2fff17ef7bfff73ef3ed677bb3da1eb9da9edfffcbfcfbf5ab5fff7adddbfb7e9bdefff8fffeb8",
+    "Block_Coinbase":"0x4838b106fce9647bdf1e7877bf73ce8b0bad5f97",
+    "Block_Date":1766188800000,
+    "Block_Difficulty":"0",
+    "Block_GasLimit":60000000,
+    "Block_GasUsed":59981801,
+    "Block_Hash":"0xacda8c801358a1d559942b1fa6d2452af100346be2cb112596a0df9c16859e9c",
+    "Block_MixDigest":"0x665e30944c3d0dc98ae0a11e214a3a87a3296448b0b5c0a49c8211add70cc862",
+    "Block_Number":24053500,
+    "Block_Nonce":0,
+    "Block_ParentHash":"0xaed2a0d62f584e12d1163d0772e09f7e6ec15757f1f6cd5885019a68e9c578af",
+    "Block_ReceiptHash":"0x853b600eb4652dd0b21ea08063b8d883b7b954018609a3c521e4a898655b7b22",
+    "Block_Result_Errors":"",
+    "Block_Result_Gas":59981801,
+    "Block_Root":"0x83db30f1d38c03b173e457ed53a4fabdd42d47078bd29b8c76fd451e7f18f68a",
+    "Block_Time":1766229923,
+    "Block_TxCount":159,
+    "Block_TxHash":"0xdd944259baf09a57b52979f842ddaa2944de0c41f279146ab9906addb7d57091",
+    "Block_UncleHash":"0x1dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d49347",
+    "Block_UnclesCount":0
+  }
+]
